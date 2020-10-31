af7640b33dd5d7da47c67a1ce097e5b020ec53ab
yql <502531008@qq.com>
2020-10-31 23:03:30 +0800

    fhs

---
+++ first.js
@@ -0,0 +1,16 @@
+var uname = "Runoob";
+var score1 = 50;
+var score2 = 42.50;
+var sum = score1 + score2;
+console.log("名字: " + uname);
+console.log("第一个科目成绩: " + score1);
+console.log("第二个科目成绩: " + score2);
+console.log("总成绩: " + sum);
+var sites = {
+    site1: "Runoob",
+    site2: "Google",
+    sayHello: function () { } // 类型模板
+};
+sites.sayHello = function () { return "hello"; };
+console.log(sites.sayHello());
+console.log("dha")
